e8c5f158eee642db00bda7568a28648eace61f8b
khansuhaib <khansuhaib@gmail.com>
2021-07-20 11:51:19 -0400

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -11,14 +11,8 @@ echo "Checking if hub is ready - $HUB_HOST"
 #	sleep 1
 #done
 
-set projectLocation=/var/jenkins_home/workspace/JAVA_BUILD
-cd %projectLocation%
-set classpath=%projectLocation%/target:%projectLocation%/lib/*
-java -DHUB_HOST=$HUB_HOST -DBROWSER=$BROWSER org.testng.TestNG %projectLocation%/search-module.xml
-
-
 # start the java command
 java -cp /var/jenkins_home/workspace/JAVA_BUILD/target/selenium-docker.jar:/var/jenkins_home/workspace/JAVA_BUILD/target/selenium-docker-tests.jar:/var/jenkins_home/workspace/JAVA_BUILD/target/libs/* \
     -DHUB_HOST=$HUB_HOST \
     -DBROWSER=$BROWSER \
-    /var/jenkins_home/workspace/JAVA_BUILD/target/org.testng.TestNG $MODULE
+    /var/jenkins_home/workspace/JAVA_BUILD/target/libs/org.testng.TestNG $MODULE
